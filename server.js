3ec796a86b7c9fc2c988293cb05b4fc2a9386358
dubiousEndCrystal <electricfuzzballofficial@gmail.com>
2026-04-08 00:13:50 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Bot is alive!'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
