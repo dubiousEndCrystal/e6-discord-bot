8ce4bc8ce46f410c6d01bf256f85c89a46ba0937
dubiousEndCrystal <electricfuzzballofficial@gmail.com>
2026-04-10 19:59:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// health check route
 app.get('/', (req, res) => {
-    res.send('Bot is alive!');
+    res.send('Bot is alive');
 });
 
 app.listen(PORT, () => {
     console.log(`Web server running on port ${PORT}`);
 });
 
-// start bot AFTER server is up
+// start discord bot AFTER server starts
 require('./bot.js');
